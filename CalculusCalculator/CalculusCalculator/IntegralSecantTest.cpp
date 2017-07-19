e75ac20191daed8f6c19b4b1dc1f7954652fc977
ekretzmann <erniekretzmann@gmail.com>
2017-07-18 20:06:40 -0500

    unknown changes
    reopened project, unknown if this will make changes.

--- CalculusCalculator/CalculusCalculator/IntegralSecantTest.cpp
+++ CalculusCalculator/CalculusCalculator/IntegralSecantTest.cpp
@@ -2,15 +2,14 @@
 tester for IntegralSecant
 Created 3/8/17 by Ernie Kretzmann
 */
-/*
+
 #include"IntegralSecant.h"
 
 int main()
 {
-	IntegralSecant test(6);
+	IntegralSecant test(7);
 	test.print();
 
 	system("pause");
 	return 0;
 }
-*/
